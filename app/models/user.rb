76a38b0b17e63a45b7f100beba587b0b5a6b0d1d
David Sarkisian <dsarkisian@plansource.com>
2019-03-09 15:16:50 -0700

    Juicy UI

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,9 +2,10 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   has_many :movies
+  has_many :comments
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  validates :email, uniqueness: true
+  validates :email, :usernamae, uniqueness: true
   validates :first_name, :last_name, length:{ in: 2..20 }
 end
